bb8c8704b2d06d795d2f4940907a88d7405a8ca1
CRob <coletista7@gmail.com>
2023-08-30 20:02:49 -0500

    Added homepage routing and design as well as a base theme

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 from flask import Flask, jsonify
 import os
+from flask import Flask, flash, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine, inspect
+
+
 
 app = Flask(__name__)
+# app.secret_key = "secret_key"
 
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3'
+# db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+    return render_template('index.html')
+
+# @app.route('/login', methods=["POST","GET"])
+
 
 
 if __name__ == '__main__':
